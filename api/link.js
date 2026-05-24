26a1b273415636e4784157734ffea52db60a2923
zenofirminx-pixel <zenofirminx@gmail.com>
2026-05-24 14:21:54 +0100

    Update link.js

--- api/link.js
+++ api/link.js
@@ -1,13 +1,45 @@
-import { generateLink } from "./linkEngine.js";
+const {
+  generateLink
+} = require('../service/linkengine');
 
-export default function handler(req, res) {
-  const { type, id } = req.query;
+export default async function handler(req, res) {
 
-  if (!type || !id) {
-    return res.status(400).json({ error: "missing params" });
+  if (req.method !== 'POST') {
+
+    return res.status(405).json({
+      error: 'Method not allowed'
+    });
+
   }
 
-  const link = generateLink(type, id);
+  try {
 
-  res.status(200).json({ link });
+    const { text } = req.body;
+
+    if (!text) {
+
+      return res.status(400).json({
+        error: 'Missing text'
+      });
+
+    }
+
+    const link =
+      generateLink(text);
+
+    return res.status(200).json({
+
+      success: true,
+      input: text,
+      link
+
+    });
+
+  } catch (err) {
+
+    return res.status(500).json({
+      error: err.message
+    });
+
+  }
 }
