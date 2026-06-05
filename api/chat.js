eb8be2a2e8169974a2f3600662901d2346c48c70
zenofirminx-pixel <zenofirminx@gmail.com>
2026-06-06 00:08:10 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -10,6 +10,7 @@ function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "https://aurx.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // <-- AJOUTE CETTE LIGNE
 }
 
 // =========================
