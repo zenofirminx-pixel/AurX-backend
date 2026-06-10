5c64a845e1c9b68aaba91df5c0ea1e7da2772740
zenofirminx-pixel <zenofirminx@gmail.com>
2026-06-10 17:42:04 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,3 @@
-import { buildPrompt } from "../lib/buildPrompt.js";
 import db from "./initMemory.js";
 import { extractMemory } from "../lib/memoryExtractor.js";
 import { saveMemory } from "../lib/saveMemory.js";
