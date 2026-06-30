87afbecd8e44afe51a258b4a0c2de14c3881e41c
zenofirminx-pixel <zenofirminx@gmail.com>
2026-06-30 19:22:37 +0100

    Delete AurXChatHandler.js

--- api/AurXChatHandler.js
+++
@@ -1,8 +0,0 @@
-import { AurXEngine } from "../aurx_core/AurXEngine.js";
-
-export const config = { maxDuration: 60 };
-
-export default async function AurXChatHandler(req, res) {
-  const engine = new AurXEngine(req, res);
-  await engine.run();
-}
